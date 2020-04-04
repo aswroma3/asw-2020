8bcb0143a40fe42fa4f74b7c2f3911314ff17a7b
lucacabibbo <luca.cabibbo@uniroma3.it>
2020-04-04 20:04:49 +0200

    Aggiornato progetto asw-825-spring-boot (logging)

--- projects/asw-825-spring-boot/e-restaurant-with-menu/src/main/java/asw/efood/restaurantservice/aoplog/RestaurantServiceLoggingAspect.java
+++ projects/asw-825-spring-boot/e-restaurant-with-menu/src/main/java/asw/efood/restaurantservice/aoplog/RestaurantServiceLoggingAspect.java
@@ -22,26 +22,30 @@ public class RestaurantServiceLoggingAspect {
 	/* metodi di log */ 
     private void logInvocation(JoinPoint joinPoint) {
         final String args = Arrays.toString(joinPoint.getArgs());
-        final String methodName = joinPoint.getSignature().toShortString().replace("(..)", "()");
-        log.info("CALL [method:{}, args:{}]", methodName, args);
+//        final String methodName = joinPoint.getSignature().toShortString().replace("(..)", "()");
+        final String methodName = joinPoint.getSignature().getName().replace("(..)", "()");
+        log.info("CALL RestaurantService.{} {}", methodName, args);
     }
 
     private void logTermination(JoinPoint joinPoint, Object retValue) {
         final String args = Arrays.toString(joinPoint.getArgs());
-        final String methodName = joinPoint.getSignature().toShortString().replace("(..)", "()");
-        log.info("RETURN [method:{}, args:{}, retValue:{}]", methodName, args, retValue.toString());
+//        final String methodName = joinPoint.getSignature().toShortString().replace("(..)", "()");
+        final String methodName = joinPoint.getSignature().getName().replace("(..)", "()");
+        log.info("     RestaurantService.{} {} -> {}", methodName, args, retValue.toString());
     }
 
     private void logVoidTermination(JoinPoint joinPoint) {
         final String args = Arrays.toString(joinPoint.getArgs());
-        final String methodName = joinPoint.getSignature().toShortString().replace("(..)", "()");
-        log.info("RETURN [method:{}, args:{}]", methodName, args);
+//        final String methodName = joinPoint.getSignature().toShortString().replace("(..)", "()");
+        final String methodName = joinPoint.getSignature().getName().replace("(..)", "()");
+        log.info("     RestaurantService.{} {} -> RETURN", methodName, args);
     }
 
     private void logException(JoinPoint joinPoint, Object exception) {
         final String args = Arrays.toString(joinPoint.getArgs());
-        final String methodName = joinPoint.getSignature().toShortString().replace("(..)", "()");
-        log.info("ERROR [method:{}, args:{}, exception:{}]", methodName, args, exception.toString());
+//        final String methodName = joinPoint.getSignature().toShortString().replace("(..)", "()");
+        final String methodName = joinPoint.getSignature().getName().replace("(..)", "()");
+        log.info("     ERROR IN RestaurantService.{} {} -> {}", methodName, args, exception.toString());
     }
 
     /* Eseguito prima dell'esecuzione del metodo */
