12199304158fd9bfa316c14c0277a54302572cb5
Vito Mezzapesa <vmezzapesa@soldo.com>
2021-01-02 23:33:45 +0100

    Replaced hsql qith mysql for ricette service

--- projects/asw-instagnam/start-mysql-servers.sh
+++ projects/asw-instagnam/start-mysql-servers.sh
@@ -4,3 +4,4 @@ echo Starting MySQL servers
 
 
 docker run --name connessioni-mysql -e MYSQL_USER=username -e MYSQL_PASSWORD=password -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=connessioni -p 3307:3306 -d mysql:8.0
+docker run --name ricette-mysql -e MYSQL_USER=username -e MYSQL_PASSWORD=password -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=ricette -p 3308:3306 -d mysql:8.0
