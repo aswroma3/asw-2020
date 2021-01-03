49a3514d2ecc989f5582c012227ed52c032ab7de
Vito Mezzapesa <vmezzapesa@soldo.com>
2021-01-03 20:40:02 +0100

    Small improvements into ricette

--- projects/asw-instagnam/ricette/src/main/java/asw/instagnam/ricette/async/RicettaMessagePublisherImpl.java
+++ projects/asw-instagnam/ricette/src/main/java/asw/instagnam/ricette/async/RicettaMessagePublisherImpl.java
@@ -14,14 +14,15 @@ public class RicettaMessagePublisherImpl implements RicettaMessagePublisher {
     private final Logger logger = Logger.getLogger(RicettaMessagePublisher.class.toString());
 
     @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private KafkaTemplate<String, RicettaCreatedEvent> kafkaTemplate;
 
     @Value("${asw.kafka.topic.out}")
     private String topic;
 
-    public void sendMessage(RicettaCompleta message) {
-        logger.info(String.format("$$$$ => Producing message: %s", message));
-        this.kafkaTemplate.send(topic, message.toString());
+    public void sendMessage(RicettaCompleta ricettaCompleta) {
+        RicettaCreatedEvent ricettaCreatedEvent = new RicettaCreatedEvent(ricettaCompleta.getId(), ricettaCompleta.getAutore(), ricettaCompleta.getTitolo());
+        logger.info(String.format("$$$$ => Producing message: %s", ricettaCreatedEvent));
+        this.kafkaTemplate.send(topic, ricettaCreatedEvent);
     }
 
 }
