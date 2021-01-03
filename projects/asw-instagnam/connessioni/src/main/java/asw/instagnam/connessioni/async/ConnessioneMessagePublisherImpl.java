54c9c7b0331fba83bdb824388aec8199d9e4325d
Vito Mezzapesa <vmezzapesa@soldo.com>
2021-01-03 21:13:15 +0100

    Fixed tests

--- projects/asw-instagnam/connessioni/src/main/java/asw/instagnam/connessioni/async/ConnessioneMessagePublisherImpl.java
+++ projects/asw-instagnam/connessioni/src/main/java/asw/instagnam/connessioni/async/ConnessioneMessagePublisherImpl.java
@@ -1,6 +1,7 @@
 package asw.instagnam.connessioni.async;
 
 import asw.instagnam.connessioni.domain.Connessione;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -16,10 +17,16 @@ public class ConnessioneMessagePublisherImpl implements ConnessioneMessagePublis
     @Autowired
     private KafkaTemplate<String, ConnessioneCreatedEvent> kafkaTemplate;
 
-    @Value("${asw.kafka.topic.out}")
+    @Value("${asw.kafka.topic.out:}")
     private String topic;
 
     public void sendMessage(Connessione connessione) {
+        if (StringUtils.isBlank(topic)) {
+            //TODO -> Questa è una decisione importante! Dal punto di vista architetturale stiamo perdendo
+            // un'informazione! Ce lo possiamo permettere? Se no, eviteremo di far salire il contesto dando un errore
+            logger.warning("Skip sending message:" + connessione + ". No topic configured");
+            return;
+        }
         ConnessioneCreatedEvent connessioneCreatedEvent = new ConnessioneCreatedEvent(connessione.getId(), connessione.getFollower(), connessione.getFollowed());
         logger.info(String.format("$$$$ => Producing message: %s", connessioneCreatedEvent));
         this.kafkaTemplate.send(topic, connessioneCreatedEvent);
--- projects/asw-instagnam/ricette/src/main/java/asw/instagnam/ricette/async/RicettaMessagePublisherImpl.java
+++ projects/asw-instagnam/ricette/src/main/java/asw/instagnam/ricette/async/RicettaMessagePublisherImpl.java
@@ -1,6 +1,7 @@
 package asw.instagnam.ricette.async;
 
 import asw.instagnam.ricette.domain.RicettaCompleta;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -16,10 +17,16 @@ public class RicettaMessagePublisherImpl implements RicettaMessagePublisher {
     @Autowired
     private KafkaTemplate<String, RicettaCreatedEvent> kafkaTemplate;
 
-    @Value("${asw.kafka.topic.out}")
+    @Value("${asw.kafka.topic.out:}")
     private String topic;
 
     public void sendMessage(RicettaCompleta ricettaCompleta) {
+        if (StringUtils.isBlank(topic)) {
+            //TODO -> Questa è una decisione importante! Dal punto di vista architetturale stiamo perdendo
+            // un'informazione! Ce lo possiamo permettere? Se no, eviteremo di far salire il contesto dando un errore
+            logger.warning("Skip sending message:" + ricettaCompleta + ". No topic configured");
+            return;
+        }
         RicettaCreatedEvent ricettaCreatedEvent = new RicettaCreatedEvent(ricettaCompleta.getId(), ricettaCompleta.getAutore(), ricettaCompleta.getTitolo());
         logger.info(String.format("$$$$ => Producing message: %s", ricettaCreatedEvent));
         this.kafkaTemplate.send(topic, ricettaCreatedEvent);
