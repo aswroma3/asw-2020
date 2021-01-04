94d0e66efde664e20785e98bd8d8009ed835d1db
Vito Mezzapesa <vmezzapesa@soldo.com>
2021-01-04 23:21:51 +0100

    used local repository to answer to RicetteSeguite

--- projects/asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/RicetteSeguiteService.java
+++ projects/asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/domain/RicetteSeguiteService.java
@@ -1,29 +1,33 @@
 package asw.instagnam.ricetteseguite.domain;
 
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
-import java.util.*; 
+import java.util.ArrayList;
+import java.util.Collection;
 
-@Service 
+@Service
 public class RicetteSeguiteService {
 
-	@Autowired 
-	private ConnessioniService connessioniService;
+    @Autowired
+    @Qualifier("connessioniServiceImpl")
+    private ConnessioniService connessioniService;
 
-	@Autowired 
-	private RicetteService ricetteService;
+    @Autowired
+    @Qualifier("ricetteServiceImpl")
+    private RicetteService ricetteService;
 
-	/* Trova le ricette (in formato breve) degli utenti seguiti da utente. */ 
-	public Collection<Ricetta> getRicetteSeguite(String utente) {
-		Collection<Ricetta> ricette = new ArrayList<>(); 
-		Collection<Connessione> connessioni = connessioniService.getConnessioniByFollower(utente); 
-		for (Connessione connessione : connessioni) {
-			String followed = connessione.getFollowed();
-			Collection<Ricetta> ricetteByFollowed = ricetteService.getRicetteByAutore(followed);
-			ricette.addAll(ricetteByFollowed);
-		}
-		return ricette; 
-	}
-	
+    /* Trova le ricette (in formato breve) degli utenti seguiti da utente. */
+    public Collection<Ricetta> getRicetteSeguite(String utente) {
+        Collection<Ricetta> ricette = new ArrayList<>();
+        Collection<Connessione> connessioni = connessioniService.getConnessioniByFollower(utente);
+        for (Connessione connessione : connessioni) {
+            String followed = connessione.getFollowed();
+            Collection<Ricetta> ricetteByFollowed = ricetteService.getRicetteByAutore(followed);
+            ricette.addAll(ricetteByFollowed);
+        }
+        return ricette;
+    }
+
 }
