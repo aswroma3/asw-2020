53d133d026900fba6cbf07138bffb3eb6f7793c6
Vito Mezzapesa <vmezzapesa@soldo.com>
2021-01-04 23:13:11 +0100

    on event save connessioni and ricette

--- projects/asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/connessioni/async/ConnessioneMessageConsumer.java
+++ projects/asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/connessioni/async/ConnessioneMessageConsumer.java
@@ -1,14 +1,26 @@
 package asw.instagnam.ricetteseguite.connessioni.async;
 
+import asw.instagnam.ricetteseguite.domain.Connessione;
+import asw.instagnam.ricetteseguite.domain.ConnessioniService;
+import asw.instagnam.ricetteseguite.rest.RicetteSeguiteController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class ConnessioneMessageConsumer {
 
+    private final Logger logger = Logger.getLogger(RicetteSeguiteController.class.toString());
+
+    @Autowired
+    ConnessioniService connessioniService;
+
     @KafkaListener(topics = "${asw.kafka.topic.connessione.in}")
-    public void list(ConnessioneCreatedEvent connessioneCreatedEvent) {
-        System.out.println("connessione event received " + connessioneCreatedEvent);
+    public void listener(ConnessioneCreatedEvent connessioneCreatedEvent) {
+        logger.info(String.format("$$$$ => Consumed message: %s", connessioneCreatedEvent));
+        connessioniService.save(new Connessione(connessioneCreatedEvent.getId(), connessioneCreatedEvent.getFollower(), connessioneCreatedEvent.getFollowed()));
     }
 
 }
--- projects/asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/ricette/async/RicettaMessageConsumer.java
+++ projects/asw-instagnam/ricette-seguite/src/main/java/asw/instagnam/ricetteseguite/ricette/async/RicettaMessageConsumer.java
@@ -1,14 +1,26 @@
 package asw.instagnam.ricetteseguite.ricette.async;
 
+import asw.instagnam.ricetteseguite.domain.Ricetta;
+import asw.instagnam.ricetteseguite.domain.RicetteService;
+import asw.instagnam.ricetteseguite.rest.RicetteSeguiteController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class RicettaMessageConsumer {
 
+    private final Logger logger = Logger.getLogger(RicetteSeguiteController.class.toString());
+
+    @Autowired
+    RicetteService ricetteService;
+
     @KafkaListener(topics = "${asw.kafka.topic.ricetta.in}")
-    public void list(RicettaCreatedEvent ricettaCreatedEvent) {
-        System.out.println("ricetta event received " + ricettaCreatedEvent);
+    public void listener(RicettaCreatedEvent ricettaCreatedEvent) {
+        logger.info(String.format("$$$$ => Consumed message: %s", ricettaCreatedEvent));
+        ricetteService.save(new Ricetta(ricettaCreatedEvent.getId(), ricettaCreatedEvent.getAutore(), ricettaCreatedEvent.getTitolo()));
     }
 
 }
